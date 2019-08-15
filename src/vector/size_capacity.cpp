205975754daebb92f90f89ee33fa7fbca2163f98
Kushail <kushailsingh98@gmail.com>
2019-08-15 11:46:57 +0200

    Exercise 2.1

--- src/vector/size_capacity.cpp
+++ src/vector/size_capacity.cpp
@@ -20,3 +20,9 @@ int main()
 
     return 0;
 }
+
+// Exercise 2.1
+// Vector capacity is the size of the array used to store the vector. 
+// Vector size is the number of elements that the vector contains which is directly controlled by the number of elements added to the vector.
+// The capacity of a vector is always equal to or greater than the vector size. The benefit of using a vector over an array is dynamic size.
+// The vector capacity grows by reallocating memory to allow more elements to be added as needed. 
