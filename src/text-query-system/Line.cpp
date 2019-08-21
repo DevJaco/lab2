201f9620ddfd8aa55ec90bddc2d080419dc50a72
DevJaco <dev2708dev@gmail.com>
2019-08-21 20:58:14 +0200

    Exercise 5.2

--- src/text-query-system/Line.cpp
+++ src/text-query-system/Line.cpp
@@ -8,9 +8,60 @@ Line::Line(const string& line)
 {
 	// Hint: some of string's member functions might come in handy here
 	// for extracting words.
+	line_ = line;
 }
 
-bool Line::contains(const Word& search_word) const
-{
-	return false;
+
+bool Line::contains(const Word& search_word) const {
+	try {
+		search_word.isQueryable();
+		if(line_.empty()){
+			return false;
+		}
+		else {
+			try {
+				Word current = Word{line_};
+				
+				if(current == search_word){
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+			catch(WordContainsSpace){
+				int current_space = -1;
+				int next_space = (signed int)line_.find(' ');
+				bool check = false;
+			
+				while(next_space != -1){
+					string current;
+					
+					current = line_.substr(current_space + 1, next_space);
+					current_space = next_space + current_space + 1;
+					next_space = line_.substr(current_space + 1).find(' ');
+					
+
+					
+					Word current_word = Word{current};
+					if(current_word == search_word){
+						check = true;
+					}
+					
+					if(next_space == -1){
+						current = line_.substr(current_space + 1);
+						if(Word{current} == search_word){
+							return true; 
+						}
+					}
+				}
+				return check;
+			}
+			
+		}
+	}
+	catch(WordIsNotQueryable) {
+		return false;
+	}
+
 }
--- src/text-query-system/Line.h
+++ src/text-query-system/Line.h
@@ -11,7 +11,9 @@ public:
 	// DO NOT MODIFY THE PUBLIC INTERFACE OF THIS CLASS
 
 	// accepts a line of text (which FileReader will provide in production code)	
+
 	Line(const string& line);
+
 	// returns true if the search word is found in the line; false otherwise
 	bool contains(const Word& search_word) const;
 
