96991c7a8cab365c26772571d92598e59b8b0d87
DevJaco <dev2708dev@gmail.com>
2019-08-21 18:17:17 +0200

    Exercise 5.3 - Paragraph Implementation

--- src/text-query-system/Paragraph.cpp
+++ src/text-query-system/Paragraph.cpp
@@ -6,9 +6,25 @@
 
 void Paragraph::addLine(const Line& line)
 {
+	paragraph_.push_back(line);
 }
 
 tuple<bool, vector<int>> Paragraph::contains(const Word& search_word) const
 {
+	bool check = false;
+	vector<int> value = {};
+	
+	if(paragraph_.empty()){
+		return {check, vector<int>{}};
+	}
+	else {
+		for(int i = 0u; i < paragraph_.size(); i++){
+			if(paragraph_[i].contains(search_word)){
+				check = true;
+				value.push_back(i+1);
+			}
+		}
+		return {check, value};
+	}
 	return {false, vector<int>{}};
 }
